c5642b87d15392d26e5b604b97c76457c6115471
ryanbuckner <ryanbuckner@gmail.com>
2021-09-11 15:20:50 -0400

    Delete life360.indigoPlugin/Contents directory

--- life360.indigoPlugin/Contents/Server Plugin/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-# basic authorization hash (base64 if you want to decode it and see the sekrets)
-
-# this is a googleable or sniffable value. i imagine life360 changes this sometimes. 
-authorization_token = "cFJFcXVnYWJSZXRyZTRFc3RldGhlcnVmcmVQdW1hbUV4dWNyRUh1YzptM2ZydXBSZXRSZXN3ZXJFQ2hBUHJFOTZxYWtFZHI0Vg=="
-
-# your username and password (hope they are secure!)
-username = "ryanbuckner@gmail.com"
-password = "Morganhall1!"
